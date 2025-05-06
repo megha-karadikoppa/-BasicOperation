edb90e69ead01f134618a653fe20524b716b2629
meghakaradikoppa <meghakaradikoppa@gmail.com>
2025-05-06 15:28:38 +0530

    save

---
+++ BasicOperation.java
@@ -0,0 +1,10 @@
+public class BasicOperation{
+    public static void main(string[] args){
+        int a = 15;
+        int b = 5;
+        int sum = a + b;
+        int diff = a - b;
+        system.out.println("Addition:" + sum);
+        system.out.println("Subtraction:" + diff);
+    }
+}
